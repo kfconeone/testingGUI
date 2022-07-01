1e2fb6a662483f08733f2f65f44d7b6db8160342
Kevin <kfconeone@gmail.com>
2022-07-01 12:20:18 +0800

    tweggfsdfsdasdsadasd

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,35 @@
 <script setup lang="ts">
 import * as THREE from "three";
 import * as dat from "lil-gui";
+
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-const gui = process.client ? new dat.GUI() : null;
-const clock = new THREE.Clock();
+
 const myCanvas = ref();
-const debugEvents = {
-    first: () => console.log("執行事件1"),
-};
+
 onMounted(() => {
     // Scene
-    console.log(mobileConsole);
-    mobileConsole.init();
     const scene = new THREE.Scene(); //建立場景
 
     // Object
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    console.log("建立Box");
-
     // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     const material = new THREE.MeshNormalMaterial();
-    console.log("建立Material");
     // Mesh
     const mesh = new THREE.Mesh(geometry, material);
-    gui.add(mesh.position, "x").name("x軸移動方向").min(0).max(5).step(0.1);
-    gui.add(mesh.position, "y").name("y軸移動方向").min(0).max(5).step(0.1);
-    gui.add(debugEvents, "first").name("啟動事件1");
+
     scene.add(mesh);
-    console.log("建立Mesh");
 
     // Sizes
     const sizes = {
         width: window.innerWidth,
         height: window.innerHeight,
     };
-    console.log("sizes: ", sizes);
     console.log(sizes);
 
     // Camera
     const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
     camera.position.z = 3;
     scene.add(camera);
-    console.log(clock);
-    console.log("建立攝影機");
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({
@@ -54,20 +41,18 @@ onMounted(() => {
     // Control
 
     //以下為最簡短操作需求
-    // const orbitControl = new OrbitControls(camera, myCanvas.value);
+    const orbitControl = new OrbitControls(camera, myCanvas.value);
 
     // Animate
     const tick = () => {
         // Render
-        // console.log("delta: ", clock.getElapsedTime());
         renderer.render(scene, camera);
     };
 
     window.addEventListener("resize", () => {
         //Controls
-        // orbitControl.update();
+        orbitControl.update();
         // Update sizes
-
         sizes.width = window.innerWidth;
         sizes.height = window.innerHeight;
         // Update renderer
@@ -78,21 +63,10 @@ onMounted(() => {
     });
     renderer.setAnimationLoop(tick);
 });
-
-function ShowDebug() {
-    mobileConsole.toggle();
-}
 </script>
 
 <template>
     <div>
-        <Head>
-            <Script src="hnl.mobileConsole.js"></Script>
-        </Head>
-
-        <div class="fixed top-0 left-0 z-50 bg-white shadow-md w-fit">
-            <button @click="ShowDebug">show debug</button>
-        </div>
         <canvas ref="myCanvas"> </canvas>
     </div>
 </template>
