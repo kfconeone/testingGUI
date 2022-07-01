f80b74a28402cccae309e3414ef5aa7f99f1590e
Kevin <kfconeone@gmail.com>
2022-07-01 12:48:17 +0800

    tasdsadsadsdsad

--- pages/index.vue
+++ pages/index.vue
@@ -86,9 +86,9 @@ function ShowDebug() {
 
 <template>
     <div>
-        <Head>
+        <!-- <Head>
             <Script src="hnl.mobileConsole.js"></Script>
-        </Head>
+        </Head> -->
 
         <div class="fixed top-0 right-0 z-50 bg-white shadow-md w-fit">
             <button @click="ShowDebug">show debug</button>
