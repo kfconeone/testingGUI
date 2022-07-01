7772f181d4ec101c589bd062b4efd12036111db6
Kevin <kfconeone@gmail.com>
2022-07-01 12:06:04 +0800

    twegg

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,21 @@
 <script setup lang="ts">
 import * as THREE from "three";
-import * as dat from "lil-gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+
 const myCanvas = ref();
 
-const gui = process.client ? new dat.GUI() : null;
-const textureLoader = new THREE.TextureLoader();
-const fontLoader = new FontLoader();
-const debugEvents = {
-    first: () => console.log("執行事件1"),
-};
-
-var text;
 onMounted(() => {
-    // console.log(mobileConsole);
-    // mobileConsole.init();
     // Scene
     const scene = new THREE.Scene(); //建立場景
-    //Lights
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 
-    scene.add(ambientLight);
     // Object
-    const fontLoader = new FontLoader();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const material = new THREE.MeshNormalMaterial();
+    // Mesh
+    const mesh = new THREE.Mesh(geometry, material);
 
-    fontLoader.load("/helvetiker_regular.typeface.json", (font) => {
-        const textGeometry = new TextGeometry("Hello Three.js", {
-            font: font,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 0.03,
-            bevelSize: 0.02,
-            bevelOffset: 0,
-            bevelSegments: 5,
-        });
-
-        textGeometry.computeBoundingBox();
-        textGeometry.center();
-
-        const textMaterial = new THREE.MeshBasicMaterial({
-            wireframe: true,
-        });
-        text = new THREE.Mesh(textGeometry, textMaterial);
-        gui.add(text.position, "x").name("x軸移動方向").min(0).max(5).step(0.1);
-        gui.add(text.position, "y").name("y軸移動方向").min(0).max(5).step(0.1);
-        gui.add(debugEvents, "first").name("啟動事件1");
-
-        scene.add(text);
-    });
+    scene.add(mesh);
 
     // Sizes
     const sizes = {
@@ -81,9 +44,6 @@ onMounted(() => {
     // Animate
     const tick = () => {
         // Render
-        if (text != null) {
-            (text as THREE.Mesh<TextGeometry, THREE.MeshBasicMaterial>).updateMatrix();
-        }
         renderer.render(scene, camera);
     };
 
@@ -99,24 +59,12 @@ onMounted(() => {
         camera.aspect = sizes.width / sizes.height; //這個值是預防圖像扭曲
         camera.updateProjectionMatrix(); //然後執行這個來更新camera內部數值
     });
-
     renderer.setAnimationLoop(tick);
 });
-
-// function ShowDebug() {
-//     mobileConsole.toggle();
-// }
 </script>
 
 <template>
     <div>
-        <Head>
-            <Script src="hnl.mobileConsole.js"></Script>
-        </Head>
-        dsadsadsad
-        <!-- <div class="fixed top-0 left-0 z-50 bg-white shadow-md w-fit">
-            <button @click="ShowDebug">show debug</button>
-        </div> -->
         <canvas ref="myCanvas"> </canvas>
     </div>
 </template>
